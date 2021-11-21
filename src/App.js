23fa3ac80fc3692151ed59ac247f754f1a71837e
Nayan <nsavaliya4295@gmail.com>
2021-11-21 13:02:53 +0100

    initial setup

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+
+function App() {
+  return (
+    <React.Fragment>
+			<ToastContainer />
+			<div>
+        <h5>Short Url</h5>
+			</div>
+		</React.Fragment>
+  );
+}
+
+export default App;
